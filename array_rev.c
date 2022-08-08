046d242f0092b9b9b2571fdc9cb718781ce661ab
JuliethAnarosa <america02anarosa@gmail.com>
2022-08-07 20:47:06 -0500

    flip the array

---
+++ array_rev.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ *  array_rev - Reverse Array
+ * @arr:Array To Reverse
+ * @len:Length Of Array
+ * Return: Void(Reverse Array)
+ */
+void array_rev(char *arr, int len)
+{
+	int i;
+	char tmp;
+
+	for (i = 0; i < (len / 2); i++)
+	{
+		tmp = arr[i];
+		arr[i] = arr[(len - 1) - i];
+		arr[(len - 1) - i] = tmp;
+	}
+}
