8df15fe050fa05e8eda670689382288c845586dc
JuliethAnarosa <america02anarosa@gmail.com>
2022-08-07 22:26:43 -0500

    Function that searches the $PATH for an executable command

---
+++ path_cmd.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * path_cmd -  Search In $PATH For Excutable Command
+ * @cmd: Parsed Input
+ * Return: 1  Failure  0  Success.
+ */
+int path_cmd(char **cmd)
+{
+	char *path, *value, *cmd_path;
+	struct stat buf;
+
+	path = _getenv("PATH");
+	value = strtok(path, ":");
+	while (value != NULL)
+	{
+		cmd_path = build(*cmd, value);
+		if (stat(cmd_path, &buf) == 0)
+		{
+			*cmd = _strdup(cmd_path);
+			free(cmd_path);
+			free(path);
+			return (0);
+		}
+		free(cmd_path);
+		value = strtok(NULL, ":");
+	}
+	free(path);
+
+	return (1);
+}
