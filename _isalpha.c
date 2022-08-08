6738bcb74a9d119e4f5566cce8024184fe6054f5
JuliethAnarosa <america02anarosa@gmail.com>
2022-08-07 20:36:16 -0500

    check if an entered character is alphabetic or not

---
+++ _isalpha.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * _isalpha - Check if Alphabtic
+ *@c: Character
+ * Return: 1 If True 0 If Not
+ */
+int _isalpha(int c)
+{
+if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
