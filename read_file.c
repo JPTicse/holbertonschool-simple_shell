d64426d4a0d0666518bb7ed16b4e9c0cd24fff46
JuliethAnarosa <america02anarosa@gmail.com>
2022-08-07 23:05:43 -0500

    Function that reads the command from the file

---
+++ read_file.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * read_file - Read Command From File
+ * @filename:Filename
+ * @argv:Program Name
+ * Return: -1 or  0
+ */
+
+void read_file(char *filename, char **argv)
+{
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	int counter = 0;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	while ((getline(&line, &len, fp)) != -1)
+	{
+		counter++;
+		treat_file(line, counter, fp, argv);
+
+	}
+	if (line)
+		free(line);
+	fclose(fp);
+	exit(0);
+}
