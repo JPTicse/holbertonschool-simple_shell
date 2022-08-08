93b29c134c13933fac144bc92e70c63ceca8ba79
JuliethAnarosa <america02anarosa@gmail.com>
2022-08-07 20:40:15 -0500

    convert an integer to a string

---
+++ _itoa.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/*
+ * _itoa - Convert Integer To Char
+ * @n: Int To Convert
+ * Return: Char Pointer
+ */
+char *_itoa(unsigned int n)
+{
+	int len = 0, i = 0;
+	char *s;
+
+	len = intlen(n);
+	s = malloc(len + 1);
+	if (!s)
+		return (NULL);
+	*s = '\0';
+    /**Algorithm to make an int to a str(s)*/
+	while (n / 10)
+	{
+		s[i] = (n % 10) + '0';
+		n /= 10;
+		i++;
+	}
+	s[i] = (n % 10) + '0';
+    /**Function to rev the str(s) cuase the previous algorithm works backwards*/
+	array_rev(s, len);
+	s[i + 1] = '\0';
+	return (s);
+}
