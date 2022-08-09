435de599c5683c699570f89efff839acac3410cc
JPTicse <jpticse@gmail.com>
2022-08-08 23:38:04 -0500

    Fixing Betty's error

--- signal_to_handel.c
+++ signal_to_handel.c
@@ -1,10 +1,11 @@
 #include "shell.h"
 
- /**
-  * signal_to_handel - function that handle ^C
-  * @sig:Captured Signal
-  * Return: Void
-  */
+/**
+ * signal_to_handel - Function that handle C
+ * @sig: Captured Signal
+ * Return: Void
+ */
+
 void signal_to_handel(int sig)
 {
 	if (sig == SIGINT)
